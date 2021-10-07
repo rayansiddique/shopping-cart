51958d0fc8204f2b1afe6f2004c7784a694b803e
rayansiddique <rayansiddique.cse@gmail.com>
2021-10-07 13:51:39 +0600

    Using- HTML,CSS,JS

---
+++ products.js
@@ -0,0 +1,26 @@
+var products = {
+  123: {
+    name : "Television",
+    desc : "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    img : "tv.png",
+    price : 1500
+  },
+  124: {
+    name : "X-Box",
+    desc : "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    img : "xbox.png",
+    price : 700
+  },
+  125: {
+    name : "Bag",
+    desc : "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    img : "bag-1.png",
+    price : 50
+  },
+  126: {
+    name : "Watch",
+    desc : "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    img : "watch.png",
+    price : 20
+  }
+};
